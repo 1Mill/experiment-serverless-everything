63374847e75434a21817549f5783c5403520fce8
1Mill <erik.ekberg@outlook.com>
2022-03-28 20:15:58 -0700

    Load package

--- src/hello-world-v0/index.js
+++ src/hello-world-v0/index.js
@@ -1,7 +1,13 @@
+const { Cloudevent } = require('@1mill/cloudevents')
+
 exports.handler = async (event, ctx) => {
-	return {
-		MY_SECRET_MESSAGE: process.env.MY_SECRET_MESSAGE,
-		event,
-		isWorking: true,
-	}
+	return new Cloudevent({
+		data: JSON.stringify({
+			MY_SECRET_MESSAGE: process.env.MY_SECRET_MESSAGE,
+			event,
+			isWorking: true,
+		}),
+		source: ctx.invokedFunctionArn,
+		type: 'fct.said-hello-to-world.v0',
+	})
 }
