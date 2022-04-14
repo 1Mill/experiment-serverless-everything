9f4f78236308bb2d835afeb604189bd6ca3002f5
1Mill <erik.ekberg@outlook.com>
2022-04-13 18:53:58 -0700

    Update terraform configuration

--- src/hello-world-v0/main.tf
+++ src/hello-world-v0/main.tf
@@ -3,9 +3,12 @@ module "serverless_docker_function" {
 	version = "0.0.4"
 
 	docker = { build = abspath(path.module) }
-	environment = { NODE_ENV = "production" }
+	environment = {
+		MY_SECRET_MESSAGE = "Hello from Terraform!"
+		NODE_ENV = "production"
+	}
 	function = {
 		name = "erik-ekberg-testing"
-		version = "v1.0.0"
+		version = "v1.0.1"
 	}
 }
