b2287e2aeec6c31c31c1bb1c7eaa9ff27d6220b8
1Mill <erik.ekberg@outlook.com>
2022-04-13 18:44:48 -0700

    Update to latest module version

--- src/hello-world-v0/main.tf
+++ src/hello-world-v0/main.tf
@@ -1,6 +1,6 @@
 module "serverless_docker_function" {
 	source  = "1Mill/serverless-docker-function/aws"
-	version = "0.0.2"
+	version = "0.0.4"
 
 	docker = { build = abspath(path.module) }
 	environment = { NODE_ENV = "production" }
