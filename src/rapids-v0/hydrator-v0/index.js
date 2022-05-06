95c005e72981a567535c394ac969df569c1794db
1Mill <erik.ekberg@outlook.com>
2022-05-05 20:04:27 -0700

    Scaffold hydrator router logic

--- src/rapids-v0/hydrator-v0/index.js
+++ src/rapids-v0/hydrator-v0/index.js
@@ -1,5 +1,47 @@
+const fs = require('fs')
 const { Cloudevent } = require('@1mill/cloudevents')
+const { Lambda } = require('@1mill/lambda')
 
-exports.handler = async (cloudevent, ctx) => {
-	return 'Hello world'
+const ROUTES_DIR = Object.freeze('./routes/')
+const WEBSOCKETS_FUNCTIONNAME = Object.freeze('rapids-v0-websockets-v0')
+
+const lambda = new Lambda({
+	accessKeyId: 'palceholder',
+	endpoint: 'http://localhost:4566/',
+	region: 'us-west-2',
+	secretAccessKey: 'palceholder',
+})
+
+exports.handler = async (cloudevent = {}, ctx) => {
+	// * Validate the invoking cloudevent arguments.
+	if (!cloudevent.id) { throw new Error('Cloudevent "id" is required') }
+	if (!cloudevent.source) { throw new Error('Cloudevent "source" is required') }
+	if (!cloudevent.type) { throw new Error('Cloudevent "type" is required') }
+
+	// * Get all file names
+	const files = fs.readdirSync(ROUTES_DIR)
+
+	// * Import all "route" objects and then only keep the routes that
+	// * have the same cloudevent type as our invoking cloudevent.
+	const routes = files
+		.map(file => require(ROUTES_DIR + file).routes)
+		.flat()
+		.filter(route => route.cloudeventType === cloudevent.type)
+
+	// * Find each unique function name so that we only invoke each lambda once.
+	// * Also, check if we need to send the cloudevent to our websockets lambda.
+	const functionNames = Array.from(new Set(routes.map(item => item.functionName)))
+	const useWebsockets = routes.some(item => item.useWebsockets)
+
+	// * Create a proimse for each lambda function we need to invoke.
+	const promises = [
+		...(useWebsockets ? [WEBSOCKETS_FUNCTIONNAME] : []),
+		...functionNames,
+	].map(functionName => lambda.invoke({ cloudevent, functionName, invocationType: 'Event' }))
+
+	// * Wait for every promise to finish, regardless of error status.
+	const results = await Promise.allSettled(promises)
+	results.filter(r => r.status === 'rejected').forEach(r => console.error(r.reason))
+
+	return true
 }
---
+++ src/rapids-v0/hydrator-v0/routes/example.js
@@ -0,0 +1,5 @@
+const routes = [
+	{ functionName: 'hello-world-v0', cloudeventType: 'cmd.say-hello.v0', useWebsockets: false }
+]
+
+module.exports = { routes }
