3fd594af1bedc202ca5263443d7ec2144b215012
1Mill <erik.ekberg@outlook.com>
2022-08-14 15:50:38 -0700

    Add ability to easily rotate CICD keys

--- infra/cicd-v0/main.tf
+++ infra/cicd-v0/main.tf
@@ -2,18 +2,30 @@ data "github_repository" "this" {
 	full_name = "1Mill/experiment-serverless-everything"
 }
 
+locals {
+	active_date = element(local.dates, 0)
+	dates = [
+		# ! When rotating, place the newest date first
+		# ! so that it is used by "local.active_date"
+		"2022-08-15",
+		# ! ---
+	]
+}
+
 module "iam_user" {
+	for_each = toset(local.dates)
+
 	source  = "terraform-aws-modules/iam/aws//modules/iam-user"
 	version = "~> 5.3.0"
 
 	create_iam_access_key = true
 	force_destroy = true
-	name = "experiment-serverless-everything-cicd-v0-2022-08-14"
+	name = "experiment-serverless-everything-cicd-v0-${each.key}"
 	password_length = 64
 }
 
 resource "github_actions_secret" "aws_access_key_id" {
-	plaintext_value = module.iam_user.iam_access_key_id
+	plaintext_value = module.iam_user[local.active_date].iam_access_key_id
 	repository = data.github_repository.this.name
 	secret_name = "CICD_V0_AWS_ACCESS_KEY_ID"
 }
@@ -25,7 +37,7 @@ resource "github_actions_secret" "aws_region" {
 }
 
 resource "github_actions_secret" "aws_secret_access_key" {
-	plaintext_value = module.iam_user.iam_access_key_secret
+	plaintext_value = module.iam_user[local.active_date].iam_access_key_secret
 	repository = data.github_repository.this.name
 	secret_name = "CICD_V0_AWS_SECRET_ACCESS_KEY"
 }
