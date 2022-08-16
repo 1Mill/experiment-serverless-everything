b7068eddb66d9409a0eb1a878d9461dfb8d225ea
1Mill <erik.ekberg@outlook.com>
2022-08-15 19:40:59 -0700

    Add INFRA prefix to secrets

--- infra/cicd-v0/main.tf
+++ infra/cicd-v0/main.tf
@@ -50,3 +50,21 @@ resource "github_actions_secret" "aws_secret_access_key" {
 	repository = data.github_repository.this.name
 	secret_name = "CICD_V0_AWS_SECRET_ACCESS_KEY"
 }
+
+resource "github_actions_secret" "aws_access_key_id" {
+	plaintext_value = module.iam_user[local.active_date].iam_access_key_id
+	repository = data.github_repository.this.name
+	secret_name = "INFRA_CICD_V0_AWS_ACCESS_KEY_ID"
+}
+
+resource "github_actions_secret" "aws_region" {
+	plaintext_value = "ca-central-1"
+	repository = data.github_repository.this.name
+	secret_name = "INFRA_CICD_V0_AWS_REGION"
+}
+
+resource "github_actions_secret" "aws_secret_access_key" {
+	plaintext_value = module.iam_user[local.active_date].iam_access_key_secret
+	repository = data.github_repository.this.name
+	secret_name = "INFRA_CICD_V0_AWS_SECRET_ACCESS_KEY"
+}
