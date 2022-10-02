b2a736ac65c1514c53283d62980f6594dd727f12
1Mill <erik.ekberg@outlook.com>
2022-10-02 10:29:17 -0700

    Emit fct.said-hello-to-world.v0 to testing-my-ws-channel-integration Ably channel

--- src/hello-world-v0/index.js
+++ src/hello-world-v0/index.js
@@ -1,6 +1,8 @@
 const { Cloudevent } = require('@1mill/cloudevents')
+const { Lambda } = require('@1mill/lambda')
 const { Sops } = require('@1mill/sops')
 
+const lambda = new Lambda({})
 const sops = new Sops({ file: 'secrets.sops.json' })
 
 exports.handler = async (cloudevent = {}, ctx = {}) => {
@@ -12,7 +14,14 @@ exports.handler = async (cloudevent = {}, ctx = {}) => {
 		}),
 		source: ctx.invokedFunctionArn,
 		type: 'fct.said-hello-to-world.v0',
-	}).origin({ cloudevent })
+	})
+	.origin({ cloudevent })
+	.wschannel({ wschannelid: 'testing-my-ws-channel-integration' })
+
+	await lambda.invoke({
+		cloudevent: ce,
+		functionName: 'rapids-v0-hydrator-v0',
+	})
 
 	return ce
 }
